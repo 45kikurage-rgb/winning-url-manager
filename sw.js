c1b90b209be632a89f38c790974eb429745c6adb
45kikurage-rgb <45kikurage@gmail.com>
2026-09-09 16:55:57 +0900

    Refine mobile layout editor and management UI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='winning-url-manager-share-v20-compact-layout-editor';
+const CACHE='winning-url-manager-share-v21-mobile-layout-font';
 const ASSETS=['./','./index.html','./share.html','./home-layout.html','./home-layout-edit.html','./home-layout-read.html','./home-layout-admin.html','./manifest.webmanifest?v=4','./icon-any.png','./icon-maskable.png'];
 const BACKUP_DB='winning-url-manager-home-layout';
 const BACKUP_STORE='backups';
