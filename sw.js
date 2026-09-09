8e68ba8d97ecd33f48d8758516e5036b311c2e81
45kikurage-rgb <45kikurage@gmail.com>
2026-09-09 16:21:22 +0900

    Compact layout editor workflow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='winning-url-manager-share-v19-layout-navigation';
+const CACHE='winning-url-manager-share-v20-compact-layout-editor';
 const ASSETS=['./','./index.html','./share.html','./home-layout.html','./home-layout-edit.html','./home-layout-read.html','./home-layout-admin.html','./manifest.webmanifest?v=4','./icon-any.png','./icon-maskable.png'];
 const BACKUP_DB='winning-url-manager-home-layout';
 const BACKUP_STORE='backups';
