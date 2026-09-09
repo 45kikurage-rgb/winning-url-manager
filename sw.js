086f3b4aafc4cdbf21ecb82da2eff13cc5bf2046
45kikurage-rgb <45kikurage@gmail.com>
2026-09-09 17:00:25 +0900

    Use full-width rows for backup file selectors

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='winning-url-manager-share-v21-mobile-layout-font';
+const CACHE='winning-url-manager-share-v22-full-width-file-rows';
 const ASSETS=['./','./index.html','./share.html','./home-layout.html','./home-layout-edit.html','./home-layout-read.html','./home-layout-admin.html','./manifest.webmanifest?v=4','./icon-any.png','./icon-maskable.png'];
 const BACKUP_DB='winning-url-manager-home-layout';
 const BACKUP_STORE='backups';
