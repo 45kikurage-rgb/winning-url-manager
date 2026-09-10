a901473c48d3ca8ca10c6c1b05d863db3e0abc88
45kikurage-rgb <45kikurage@gmail.com>
2026-09-10 14:35:50 +0900

    Bump cache for bottom dock order fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='winning-url-manager-share-v43-dock-3col';
+const CACHE='winning-url-manager-share-v44-dock-order';
 const ASSETS=['./','./index.html','./share.html','./home-layout.html','./home-layout-edit.html','./home-layout-read.html','./home-layout-admin.html','./home-layout-monthly-history.html','./fonts/Corporate-Logo-Rounded-Bold-ver3.woff2','./manifest.webmanifest?v=4','./icon-any.png','./icon-maskable.png'];
 const BACKUP_DB='winning-url-manager-home-layout';
 const BACKUP_STORE='backups';
