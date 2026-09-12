45a7f4d0b25dd5a3817129949dd4f11a489755b7
45kikurage-rgb <45kikurage@gmail.com>
2026-09-13 05:45:41 +0900

    Refresh PWA cache for special bulk copy buttons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='winning-url-manager-share-v67-row7-folders';
+const CACHE='winning-url-manager-share-v68-special-bulk-copy';
 const ASSETS=['./','./index.html','./share.html','./home-layout.html','./home-layout-edit.html','./home-layout-marker-core.js?v=6','./home-layout-read.html','./home-layout-admin.html','./home-layout-monthly-history.html','./cokeon-flow.js?v=2','./airwallet-flow.js?v=2','./fonts/Corporate-Logo-Rounded-Bold-ver3.woff2','./manifest.webmanifest?v=4','./icon-any.png','./icon-maskable.png'];
 const BACKUP_DB='winning-url-manager-home-layout';
 const BACKUP_STORE='backups';
