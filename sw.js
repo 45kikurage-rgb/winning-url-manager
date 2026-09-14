0d5167c632d3a038b23a505ca01d37bb5e44502f
45kikurage-rgb <45kikurage@gmail.com>
2026-09-14 13:19:59 +0900

    やかんの麦茶カードを旧APIでも動作させる
    * Fix Yakkan CokeON card on legacy API
    
    * Refresh CokeON frontend cache
    
    * Test legacy CokeON compatibility

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="winning-url-manager-20260914-explicit-sub-device";
-const ASSETS=['./device-access.js?v=4','./layout-account-reset-ui.js?v=2','./revenue-deduction.js?v=2','./','./index.html','./share.html','./home-layout.html','./home-layout-edit.html','./home-layout-marker-core.js?v=8','./home-layout-read.html','./home-layout-admin.html','./home-layout-monthly-history.html','./cokeon-flow.js?v=2','./airwallet-flow.js?v=2','./fonts/Corporate-Logo-Rounded-Bold-ver3.woff2','./manifest.webmanifest?v=20260914-white-splash','./icon-transparent-192.png?v=20260914-white-splash','./icon-transparent-512.png?v=20260914-white-splash','./icon-maskable.png?v=20260914-white-splash'];
+const CACHE="winning-url-manager-20260914-yakkan-compat";
+const ASSETS=['./device-access.js?v=4','./layout-account-reset-ui.js?v=2','./revenue-deduction.js?v=2','./','./index.html','./share.html','./home-layout.html','./home-layout-edit.html','./home-layout-marker-core.js?v=8','./home-layout-read.html','./home-layout-admin.html','./home-layout-monthly-history.html','./cokeon-flow.js?v=3','./airwallet-flow.js?v=2','./fonts/Corporate-Logo-Rounded-Bold-ver3.woff2','./manifest.webmanifest?v=20260914-white-splash','./icon-transparent-192.png?v=20260914-white-splash','./icon-transparent-512.png?v=20260914-white-splash','./icon-maskable.png?v=20260914-white-splash'];
 const BACKUP_DB='winning-url-manager-home-layout';
 const BACKUP_STORE='backups';
 
@@ -27,7 +27,7 @@ const INDEX_DOCK_AFTER=`<div class="bottomDock">
     <button id="moreOperationsBtn" class="moreOperationsBtn" type="button">▶ その他操作</button>
   </div>`;
 
-const COKEON_SCRIPT='<script src="./cokeon-flow.js?v=2"></script>';
+const COKEON_SCRIPT='<script src="./cokeon-flow.js?v=3"></script>';
 const AIRWALLET_SCRIPT='<script src="./airwallet-flow.js?v=2"></script>';
 
 function openBackupDb(){
