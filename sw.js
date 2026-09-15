322caa1c8546603d74623db247d107ef3c37bf67
45kikurage-rgb <45kikurage@gmail.com>
2026-09-15 19:15:25 +0900

    Refresh PWA cache for horizontal output buttons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="winning-url-manager-20260915-compact-cards-v3";
+const CACHE="winning-url-manager-20260915-compact-cards-v4";
 const ASSETS=['./device-access.js?v=7','./button-display-mode.js?v=1','./layout-account-reset-ui.js?v=2','./revenue-deduction.js?v=2','./','./index.html','./share.html','./home-layout.html','./home-layout-edit.html','./home-layout-marker-core.js?v=8','./home-layout-read.html','./home-layout-admin.html','./home-layout-monthly-history.html','./fonts/Corporate-Logo-Rounded-Bold-ver3.woff2','./manifest.webmanifest?v=20260914-white-splash','./icon-transparent-192.png?v=20260914-white-splash','./icon-transparent-512.png?v=20260914-white-splash','./icon-maskable.png?v=20260914-white-splash'];
 const BACKUP_DB='winning-url-manager-home-layout';
 const BACKUP_STORE='backups';
